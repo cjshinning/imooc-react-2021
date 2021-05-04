1fa9ec13ef7ce80597166e8f71ae9dad17a669a8
cjshinningg <784319417@qq.com>
2021-05-04 23:48:58 +0800

    2-2 es6常用语法

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './es6';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.render(<React.StrictMode >
+  <App />
+</React.StrictMode>,
   document.getElementById('root')
 );
 
