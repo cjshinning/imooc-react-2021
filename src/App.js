e9a8fcc6cfe09d33fa5748c3a8dbc062b5fc85a1
cjshinningg <784319417@qq.com>
2021-05-05 14:10:51 +0800

    3-4 React基础回顾-事件

--- src/App.js
+++ src/App.js
@@ -21,15 +21,23 @@ class Yiying extends React.Component {
   constructor(props) {
     super();
     this.state = {
-      solders: ['虎子', '柱子', '王根生']
+      soldiers: ['虎子', '柱子', '王根生']
     }
+    // this.addSoldier = this.addSoldier.bind(this);
+  }
+  addSoldier() {
+    console.log('hello add soldier');
+    this.setState({
+      soldiers: [...this.state.soldiers, '新兵蛋子' + Math.random()]
+    })
   }
   render() {
     return (
       <div>
         <h2>一营营长，{this.props.laoda}</h2>
+        <button onClick={() => this.addSoldier()}>新兵入伍</button>
         <ul>
-          {this.state.solders.map(v => {
+          {this.state.soldiers.map(v => {
             return <li key={v}>{v}</li>
           })}
         </ul>
