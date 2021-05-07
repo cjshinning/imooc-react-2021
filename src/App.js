e3b347e039146208f028eb5fc76c54b63e5e20ed
cjshinningg <784319417@qq.com>
2021-05-07 22:41:14 +0800

    4-3 Redux状态管理-组件解耦

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { addGun } from './index.redux';
 
 class App extends React.Component {
   render() {
-    const num = this.props.store.getState();
+    const store = this.props.store;
+    const num = store.getState();
+    const addGun = this.props.addGun;
+    const removeGun = this.props.removeGun;
     return (
       <div>
         <h1>现在有机关枪{num}把</h1>
-        <button onClick={() => { this.props.store.dispatch(addGun()) }}>加机关枪</button>
+        <button onClick={() => { store.dispatch(addGun()) }}>申请武器</button>
+        <button onClick={() => { store.dispatch(removeGun()) }}>上交武器</button>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
-import { Counter } from './index.redux';
+import { Counter, addGun, removeGun } from './index.redux';
 import App from './App';
 
 let store = createStore(Counter);
 
 function render() {
-  ReactDOM.render(<App store={store} />, document.getElementById('root'));
+  ReactDOM.render(<App store={store} addGun={addGun} removeGun={removeGun} />, document.getElementById('root'));
 }
 
 render();
