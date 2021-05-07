c41473c0d2fe28169cc6562d21e9d1eb465201e0
cjshinningg <784319417@qq.com>
2021-05-07 23:27:21 +0800

    4-5 Redux状态管理-插件安装

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { Counter, addGun, removeGun, addGunAsync } from './index.redux';
 import App from './App';
 
-let store = createStore(Counter, applyMiddleware(thunk));
+let store = createStore(Counter, compose(
+  applyMiddleware(thunk),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+));
 
 function render() {
   ReactDOM.render(<App store={store} addGun={addGun} removeGun={removeGun} addGunAsync={addGunAsync} />, document.getElementById('root'));
