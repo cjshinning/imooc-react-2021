460d8759b95ac63fa43722249e0c2a971fa2b562
cjshinningg <784319417@qq.com>
2021-05-09 12:50:31 +0800

    4-8 路由-初识React-router4

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import { Counter } from './index.redux';
 import App from './App';
 
@@ -11,8 +17,33 @@ let store = createStore(Counter, compose(
   window.devToolsExtension ? window.devToolsExtension() : f => f
 ));
 
+function Erying() {
+  return <h2>二营</h2>
+}
+
+function Qibinglian() {
+  return <h2>骑兵连</h2>
+}
+
 ReactDOM.render(
   (<Provider store={store}>
-    <App />
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">一营</Link>
+          </li>
+          <li>
+            <Link to="/erying">二营</Link>
+          </li>
+          <li>
+            <Link to="/qibinglian">骑兵连</Link>
+          </li>
+        </ul>
+        <Route path="/" exact component={App}></Route>
+        <Route path="/erying" component={Erying}></Route>
+        <Route path="/qibinglian" component={Qibinglian}></Route>
+      </div>
+    </Router>
   </Provider>), document.getElementById('root')
 );
