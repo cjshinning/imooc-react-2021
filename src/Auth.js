b415dbbe01b4ef830c5d8983503ebf2d6f7bfdb0
cjshinningg <784319417@qq.com>
2021-05-16 12:21:39 +0800

    5-2 前后端联调1

--- src/Auth.js
+++ src/Auth.js
@@ -1,18 +1,36 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { login } from './Auth.redux';
+import { login, getUserData } from './Auth.redux';
 import {
   Redirect
 } from "react-router-dom";
+import axios from 'axios';
 
 @connect(
   state => state.Auth,
-  { login },
+  { login, getUserData },
 )
 class Auth extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     data: {}
+  //   }
+  // }
+  componentDidMount() {
+    // axios.get('/data')
+    //   .then((res) => {
+    //     console.log(res);
+    //     if (res.status === 200) {
+    //       this.setState({ data: res.data })
+    //     }
+    //   })
+    this.props.getUserData();
+  }
   render() {
     return (
       <div>
+        <h2>我的名字是：{this.props.user}，年龄：{this.props.age}</h2>
         {this.props.isAuth ? <Redirect to="/dashboard" /> : null}
         <h2>您还没有权限，需求登录才能查看</h2>
         <button onClick={this.props.login}>登录</button>
