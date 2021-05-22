f66a15f7653773deee8a393ebc235d0d7fadc845
cjshinningg <784319417@qq.com>
2021-05-22 23:11:46 +0800

    6-9 登录注册-注册前后端联调

--- server/model.js
+++ server/model.js
@@ -1,8 +1,36 @@
 
-// const mongoose = require('mongoose');
-// // 连接mongo，并且使用imooc2021
-// const DB_URL = 'mongodb://127.0.0.1:27017/imooc2021';
-// mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+const mongoose = require('mongoose');
+// 连接mongo，并且使用imooc集合
+const DB_URL = 'mongodb://127.0.0.1:27017/imooc-chat-2021';
+mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const models = {
+  user: {
+    'user': { type: String, require: true },
+    'pwd': { type: String, require: true },
+    'type': { type: String, require: true },
+    // 头像
+    'avatar': { type: String },
+    // 个人简介或职位简介
+    'desc': { type: String },
+    // 职位名
+    'title': { type: String },
+    // 如果你是boss，还有两个字段
+    'company': { type: String },
+    'money': { type: String }
+  },
+  chat: {}
+}
+
+for (let m in models) {
+  mongoose.model(m, new mongoose.Schema(models[m]));
+}
+
+module.exports = {
+  getModel: function (name) {
+    return mongoose.model(name);
+  }
+}
 
 // mongoose.connection.on('connected', () => {
 //   console.log('mongo connected success');
