4a54d0f36b5ed734d0229c17f106bf0fb532ee3b
cjshinningg <784319417@qq.com>
2021-05-23 12:59:54 +0800

    6-11 登录注册-登录注册实现

--- src/container/login/login.js
+++ src/container/login/login.js
@@ -1,27 +1,53 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 import Logo from '../../component/logo/logo';
 import { List, InputItem, Button, WhiteSpace, WingBlank } from 'antd-mobile';
+import { connect } from 'react-redux';
+import { login } from '../../redux/user.redux';
 
+@connect(
+  state => state.user,
+  { login }
+)
 class Login extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      user: '',
+      pwd: ''
+    }
     this.register = this.register.bind(this);
+    this.handlerLogin = this.handlerLogin.bind(this);
   }
   register() {
     this.props.history.push('/register');
   }
+  handlerChange(key, val) {
+    this.setState({
+      [key]: val
+    });
+  }
+  handlerLogin() {
+    this.props.login(this.state);
+  }
   render() {
     return (
       <div>
+        {this.props.redirectTo ? <Redirect to={this.props.redirectTo} /> : null}
         <Logo></Logo>
         <h2>登录页</h2>
         <WingBlank>
           <List>
-            <InputItem>用户</InputItem>
-            <InputItem>密码</InputItem>
+            {this.props.msg ? <p className='error-msg'>{this.props.msg}</p> : null}
+            <InputItem
+              onChange={(v) => { this.handlerChange('user', v) }}
+            >用户</InputItem>
+            <InputItem
+              onChange={(v) => { this.handlerChange('pwd', v) }}
+            >密码</InputItem>
           </List>
           <WhiteSpace></WhiteSpace>
-          <Button type="primary">登录</Button>
+          <Button type="primary" onClick={this.handlerLogin}>登录</Button>
           <WhiteSpace></WhiteSpace>
           <Button type="primary" onClick={this.register}>注册</Button>
         </WingBlank>
