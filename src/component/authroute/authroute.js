bdbbd050162d8c75516d33f70f5e3b0036af2a10
cjshinningg <784319417@qq.com>
2021-05-23 17:31:12 +0800

    6-12 登录注册-cookie保存状态

--- src/component/authroute/authroute.js
+++ src/component/authroute/authroute.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import axios from 'axios';
 import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { loadData } from '../../redux/user.redux';
 
 @withRouter
+@connect(
+  null,
+  { loadData }
+)
 class AuthRoute extends React.Component {
   componentDidMount() {
     // console.log(this.props);
@@ -12,15 +18,16 @@ class AuthRoute extends React.Component {
       return null;
     }
     // 获取用户信息
+    // this.props.userinfo();
     axios.get('/user/info')
       .then(res => {
         if (res.status === 200) {
           if (res.data.code === 0) {
             // 有登录信息
+            this.props.loadData(res.data.data);
           } else {
             this.props.history.push('/login');
           }
-          // console.log(res.data);
         }
       })
 
--- src/redux/user.redux.js
+++ src/redux/user.redux.js
@@ -3,13 +3,13 @@ import { getRedirectPath } from '../util';
 const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
 const ERROR_MSG = 'ERROR_MSG';
+const LOAD_DATA = 'LOAD_DATA';
 
 const initState = {
   redirectTo: '',
   isAuth: false,
   msg: '',
   user: '',
-  pwd: '',
   type: ''
 }
 
@@ -19,6 +19,8 @@ export function user(state = initState, action) {
       return { ...state, msg: '', redirectTo: getRedirectPath(action.payload), isAuth: true, ...action.payload };
     case LOGIN_SUCCESS:
       return { ...state, msg: '', redirectTo: getRedirectPath(action.payload), isAuth: true, ...action.payload };
+    case LOAD_DATA:
+      return { ...state, ...action.payload };
     case ERROR_MSG:
       return { ...state, isAuth: false, msg: action.msg };
     default:
@@ -37,6 +39,28 @@ function loginSuccess(data) {
 function registerSuccess(data) {
   return { type: REGISTER_SUCCESS, payload: data };
 }
+
+export function loadData(data) {
+  return { type: LOAD_DATA, payload: data }
+}
+
+// export function userinfo() {
+//   return dispatch => {
+//     axios.get('/user/info')
+//       .then(res => {
+//         if (res.status === 200) {
+//           if (res.data.code === 0) {
+//             // 有登录信息
+//             dispatch(loadData(res.data.data))
+//           } else {
+//             // 注意：这里this.props会报错
+//             // this.props.history.push('/login');
+//           }
+//           // console.log(res.data);
+//         }
+//       })
+//   }
+// }
 
 export function login({ user, pwd }) {
   if (!user || !pwd) {
@@ -65,6 +89,7 @@ export function register({ user, pwd, repeatpwd, type }) {
   return dispatch => {
     axios.post('/user/register', { user, pwd, type })
       .then(res => {
+        console.log(123)
         if (res.status === 200 && res.data.code === 0) {
           dispatch(registerSuccess({ user, pwd, type }));
         } else {
