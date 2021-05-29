4de769817bf70d6571dd8c3d44a757cc0e5cbe87
cjshinningg <784319417@qq.com>
2021-05-29 22:42:25 +0800

    8-4 牛人列表-使用redux管理牛人列表

--- src/component/boss/boss.js
+++ src/component/boss/boss.js
@@ -1,7 +1,12 @@
 import React from 'react';
-import axios from 'axios';
+import { connect } from 'react-redux';
 import { Card, WingBlank, WhiteSpace } from 'antd-mobile';
+import { getUserList } from '../../redux/chatuser.redux';
 
+@connect(
+  state => state.chatuser,
+  { getUserList }
+)
 class Boss extends React.Component {
   constructor() {
     super();
@@ -10,23 +15,14 @@ class Boss extends React.Component {
     }
   }
   componentDidMount() {
-    axios.get('user/list', {
-      params: {
-        type: 'genius'
-      }
-    }).then(res => {
-      if (res.data.code === 0) {
-        this.setState({ data: res.data.data });
-      }
-    })
+    this.props.getUserList('genius');
   }
   render() {
-    console.log(this.state);
     const Header = Card.Header;
     const Body = Card.Body;
     return (
       <WingBlank>
-        {this.state.data.map(v => (
+        {this.props.userlist.map(v => (
           v.avatar ? <Card key={v._id}>
             <Header
               title={v.user}
