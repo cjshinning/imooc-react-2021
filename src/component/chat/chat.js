1b5f8b74ace180f1f88b890e32c5ad26047ccdbf
cjshinningg <784319417@qq.com>
2021-06-14 17:39:50 +0800

    10- 聊天未读消息实时展示

--- src/component/chat/chat.js
+++ src/component/chat/chat.js
@@ -15,8 +15,6 @@ class Chat extends React.Component {
     this.state = { text: '', msg: [] }
   }
   componentDidMount() {
-    this.props.getMsgList();
-    this.props.recvMsg();
     // socket.on('recvmsg', (data) => {
     //   this.setState({
     //     msg: [...this.state.msg, data.text]
