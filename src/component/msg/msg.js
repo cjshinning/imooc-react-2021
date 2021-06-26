d683a85b83cf8ac895204fc8e21fa89eaee96e62
cjshinningg <784319417@qq.com>
2021-06-26 13:15:49 +0800

    11-4 最新消息排序

--- src/component/msg/msg.js
+++ src/component/msg/msg.js
@@ -20,11 +20,18 @@ class Msg extends React.Component {
       msgGroup[v.chatid] = msgGroup[v.chatid] || [];
       msgGroup[v.chatid].push(v);
     })
-    const chatList = Object.values(msgGroup);
+    const chatList = Object.values(msgGroup).sort((a, b) => {
+      const a_last = this.getLast(a).create_time;
+      const b_last = this.getLast(b).create_time;
+      return b_last - a_last;
+    });
+    // 1. ESLint代码校验
+    // 2. react16特有的错误处理机制
+    // 3. react性能优化
     return (
       <div>
         {chatList.map(v => {
-          console.log(v);
+          // console.log(v);
           const lastItem = this.getLast(v);
           const unreadNum = v.filter(v => !v.read && v.to === userid).length;
           const targetId = v[0].from === userid ? v[0].to : v[0].from;
@@ -40,6 +47,10 @@ class Msg extends React.Component {
               <Item
                 extra={<Badge text={unreadNum} />}
                 thumb={require(`../img/${userInfo[targetId].avatar}.png`).default}
+                arrow='horizontal'
+                onClick={() => {
+                  this.props.history.push(`/chat/${targetId}`);
+                }}
               >
                 {lastItem.content}
                 <Brief>{userInfo[targetId].name}</Brief>
